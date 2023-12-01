69484c5d65431cbf8c4941552ca7c86faedea1dd
museeskinder <eskindermuse@gmail.com>
2023-12-01 15:17:20 +0300

    removeFromArray first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, element) {
+    return arr.filter((arrElement) => arrElement !== element);
 };
 
 // Do not edit below this line
