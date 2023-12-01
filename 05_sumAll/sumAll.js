6fee604a37a9d1e9d9f81cacac3866c47faab1d5
museeskinder <eskindermuse@gmail.com>
2023-12-01 15:46:03 +0300

    sumAll test 1 passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
 
+    if(typeof(num1) === "number"  
+        && typeof(num2) === "number" 
+        && num1 > 0
+        && num2 > 0) {
+            for(let i = num1; i <= num2; i++) {
+                result += i;
+            }
+            return result;
+        }
+    else
+        return "ERROR";
 };
 
 // Do not edit below this line
