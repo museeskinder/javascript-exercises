daa77acf6bea57c123257798579c1848cab4aa5d
museeskinder <eskindermuse@gmail.com>
2023-12-01 14:24:18 +0300

    repeatingString finished with all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatingNum) {
+    let store = word;
+    for(let i = 0; i < repeatingNum - 1; i++) {
+        store += word
+    }
+    return store;
 };
 
 // Do not edit below this line
