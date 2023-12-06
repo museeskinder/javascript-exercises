22e8bd6a1c4063b9616841dd9441f30892d47908
museeskinder <eskindermuse@gmail.com>
2023-12-06 12:01:45 +0300

    tempConversion: all test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Number.isInteger(((temp - 32) * 5 / 9)) ? ((temp - 32) * 5 / 9) : Number(((temp - 32) * 5 / 9).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Number.isInteger(((temp * (9 / 5)) + 32)) ? ((temp * (9 / 5)) + 32) : Number(((temp * (9 / 5)) + 32).toFixed(1));
 };
+
+convertToCelsius(-100);
 
 // Do not edit below this line
 module.exports = {
